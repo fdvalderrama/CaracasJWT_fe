af98ce3607bac377f87f332ca5c4b5778e219196
fersizes <mobileapps@sizesandcolors.com>
2025-12-09 13:51:32 -0600

    Login sin uso de jwt terminado

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import type { User } from "../types/User";
 
 const Users = () => {
-  return <div>Users</div>;
+  const users: User[] = [
+    { id: 1, name: "Daniel", email: "daniel@example.com", role: "admin" },
+    { id: 2, name: "Fernando", email: "fernando@example.com", role: "cajero" },
+  ];
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold mb-10">Users</h1>
+      <table>
+        <thead>
+          <tr className="bg-gray-300">
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr className="text-center" key={user.id}>
+              <td className="px-2">{user.name}</td>
+              <td className="px-2">{user.email}</td>
+              <td className="px-2">{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Users;
