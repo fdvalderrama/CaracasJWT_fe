e53fd0281567153a0c6214349049b3960ab729d7
fersizes <mobileapps@sizesandcolors.com>
2025-12-10 11:50:20 -0600

    Modulo home terminado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import api from "../services/axiosConfig";
+import { useNavigate } from "react-router-dom";
+import Loading from "../components/Loading";
 
 const Home = () => {
-  useEffect(() => {
-    const validateToken = "";
-  });
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
 
-  return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-6xl font-bold">Home</h1>
-    </div>
-  );
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        await api.get("/Auth");
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    validateToken();
+  }, []);
+
+  const logout = () => {
+    localStorage.setItem("token", "");
+    navigate("/");
+  };
+
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <h1 className="text-4xl font-bold mb-5">Home</h1>
+        <button
+          onClick={logout}
+          className="bg-gray-400 px-2 py-3 rounded-4xl cursor-pointer active:bg-gray-600"
+        >
+          Log out
+        </button>
+      </div>
+    );
+  }
 };
 
 export default Home;
